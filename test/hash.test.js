7dbeddf9281169dae968c0e325245abd4ddeca10
Justin McKay <1891757+jcmckay@users.noreply.github.com>
2018-08-14 10:36:29 -0700

    added a few more test cases for undo and redo

--- test/hash.test.js
+++ test/hash.test.js
@@ -140,5 +140,51 @@ describe('Hash', function() {
       expect(hash.redo.bind(hash)).to.throw('Nothing to Redo');
     });
 
+    it('should redo after an undo', function() {
+      hash = new Hash();
+      hash.set(4,7);
+      expect(hash.get(4)).to.equal(7);
+      hash.undo();
+      expect(hash.get(4)).to.equal(undefined);
+      hash.redo();
+      expect(hash.get(4)).to.equal(7);
+    });
+
+    it('should redo multiples after a undoing multples', function() {
+      hash = new Hash();
+      hash.set(4,1);
+      hash.set(4,2);
+      hash.set(4,3);
+      hash.set(4,4);
+
+      expect(hash.get(4)).to.equal(4);
+      hash.undo();
+      expect(hash.get(4)).to.equal(3);
+      hash.undo();
+      expect(hash.get(4)).to.equal(2);
+      hash.undo();
+      expect(hash.get(4)).to.equal(1);
+      hash.undo();
+      expect(hash.get(4)).to.equal(undefined);
+      hash.redo();
+      expect(hash.get(4)).to.equal(1);
+      hash.redo();
+      expect(hash.get(4)).to.equal(2);
+      hash.redo();
+      expect(hash.get(4)).to.equal(3);
+      hash.redo();
+      expect(hash.get(4)).to.equal(4);
+    });
+
+    it('should undo and redo deletes', function() {
+      hash = new Hash();
+      hash.set(4,1);
+      expect(hash.get(4)).to.equal(1);
+      hash.delete(4);
+      expect(hash.get(4)).to.equal(undefined);
+      hash.undo();
+      expect(hash.get(4)).to.equal(1);
+    });
+
   });
 });
